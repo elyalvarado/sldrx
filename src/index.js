90d07d628c2746a87467c629d4bcdc6ea3d0a8a7
Ely Alvarado <elyalvarado@gmail.com>
2018-08-03 15:44:21 -0500

    Rename data attribute used as selector

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ let poly = require("preact-cli/lib/lib/webpack/polyfills");
 import { h } from "preact";
 import habitat from "preact-habitat";
 
-import Widget from "./components/SlideReactions";
+import SlideReactions from "./components/SlideReactions";
 
-let _habitat = habitat(Widget);
+let _habitat = habitat(SlideReactions);
 
 _habitat.render({
-  selector: '[data-widget-host="habitat"]',
+  selector: '[data-widget-sldrx]',
   clean: true
 });
