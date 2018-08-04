64c86a5887e78e2ed5cca804aa54423dbbc187e1
Ely Alvarado <elyalvarado@gmail.com>
2018-08-03 19:11:05 -0500

    Refactor ReactionsContainer to functional component

--- src/components/ReactionsContainer.js
+++ src/components/ReactionsContainer.js
@@ -1,18 +1,16 @@
 import { h, cloneElement, Component } from "preact";
 
-export default class ReactionsContainer extends Component {
-  render(props) {
-    const style = {
-      fontSize: '30px',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }
-
-    return (
-      <div style={style}>
-        { props.children.map(child => cloneElement(child, this.props)) }
-      </div>
-    )
-  }
+const style = {
+  fontSize: '30px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
 }
+
+const ReactionsContainer = (props) => (
+  <div style={style}>
+    { props.children.map(child => cloneElement(child, props)) }
+  </div>
+)
+
+export default ReactionsContainer
