97e219af7ad2b478ee0b2f29ce700dd34010f2d9
Ely Alvarado <elyalvarado@gmail.com>
2018-08-03 22:39:15 -0500

    Remove credentials hardcoded in template file

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,18 @@
+import { DefinePlugin } from 'webpack';
+require('dotenv').config()
+
+const { FIREBASE_API_KEY, FIREBASE_DB_URL } = process.env
+
 export default (config, env, helpers) => {
   delete config.entry.polyfills;
   config.output.filename = "[name].js";
+
+  config.plugins.push(
+    new DefinePlugin({
+      'process.env.FIREBASE_API_KEY': JSON.stringify(FIREBASE_API_KEY),
+      'process.env.FIREBASE_DB_URL': JSON.stringify(FIREBASE_DB_URL)
+    })
+  );
 
   let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
   plugin.options.disable = true;
